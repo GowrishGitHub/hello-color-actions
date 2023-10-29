75a429ae998222228b5271a3bc247f8d86adb878
GowrishGitHub <146477026+GowrishGitHub@users.noreply.github.com>
2023-10-29 14:31:43 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Tornto Canada - 2time";
+const hello = () => "Hello World from the Tornto Canada - 2nd time";
 console.log(hello());
 exports.hello = hello;
